8727900c5601183013e98eea6231b6d6f59b33e4
Arian Picco <arianpicco@gmail.com>
2025-07-31 22:11:58 -0300

    test

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/tandil-guide-2/',
-  build: {
-    outDir: 'docs' // ← this is the key!
-  },
   plugins: [tailwindcss()]
-})
+});
+
